095510642d697f3be63b61b64f5517ec6c085975
Davimalu <accounts@zeugner.dev>
2025-04-10 13:41:20 +0200

    Refactor UI components to use WPF-UI controls various Views; Implement dark mode (buggy)

--- TourPlanner/Views/MenuBar.xaml.cs
+++ TourPlanner/Views/MenuBar.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wpf.Ui.Controls;
 
 namespace TourPlanner.Views
 {
@@ -24,5 +25,35 @@ namespace TourPlanner.Views
         {
             InitializeComponent();
         }
+        
+        /// <summary>
+        /// handles switching between light and dark theme
+        /// </summary>
+        private void ThemeToggleSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            // This method is directly dependent on WPF / WPF-UI, thus it can reside in the Code Behind
+            bool isDarkTheme = ThemeToggleSwitch.IsChecked ?? false;
+            
+            if (isDarkTheme)
+            {
+                // Apply Dark Theme
+                Wpf.Ui.Appearance.ApplicationThemeManager.Apply(
+                    Wpf.Ui.Appearance.ApplicationTheme.Dark,
+                    Wpf.Ui.Controls.WindowBackdropType.None,
+                    true
+                );
+            }
+            else
+            {
+                // Apply Light Theme
+                Wpf.Ui.Appearance.ApplicationThemeManager.Apply(
+                    Wpf.Ui.Appearance.ApplicationTheme.Light,
+                    Wpf.Ui.Controls.WindowBackdropType.None, // For whatever reason, this is the only way to get the light theme to work | https://github.com/lepoco/wpfui/issues/1222
+                    true
+                );
+            }
+        }
+        
+        // Theme Switching seems to be really buggy | https://github.com/lepoco/wpfui/issues
     }
 }
