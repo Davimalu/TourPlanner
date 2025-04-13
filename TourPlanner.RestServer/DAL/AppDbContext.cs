8dceec4425c01c5657bf7603172482f5e3ba7bc4
Davimalu <44408181+Davimalu@users.noreply.github.com>
2025-04-13 14:49:44 +0200

    Persist data in a database using the Entity Framework Core (#10)
    * Add Entity Framework Core and PostgreSQL packages; create DAL folder
    
    * Add persistence via a PostgreSQL database using Entity Framework Core
    
    * Don't require Logs for a Tour; Add script for adding dummy Tours

---
+++ TourPlanner.RestServer/DAL/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TourPlanner.RestServer.Models;
+
+namespace TourPlanner.RestServer.DAL;
+
+public class AppDbContext : DbContext
+{
+    // Constructor supporting dependency injection
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    
+    
+    // Define the DbSets for the entities
+    public DbSet<Tour> Tours { get; set; }
+    // We explicitly declare a DBSet for TourLogs so that it can be queried on its own (otherwise the table would still be created as the TourModel requires it,
+    // but we couldn't query it directly since there would be no DbSet / Context for it)
+    public DbSet<TourLog> TourLogs { get; set; }
+    
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Saving the DateTime to the database in local time throws an error (and could lead to inconsistencies), so we need to convert it to UTC before saving
+        modelBuilder.Entity<TourLog>()
+            .Property(tl => tl.TimeStamp)
+            .HasConversion(
+                // Convert the value to UTC before saving:
+                v => v.ToUniversalTime(),
+                // When reading from the database, mark as UTC:
+                v => DateTime.SpecifyKind(v, DateTimeKind.Utc)
+            );
+
+        // Call the base method to ensure any other configurations are applied (our OnModelCreating method overrides the base method of the EntityFrameworkCore DbContext,
+        // so if we still want the logic in the original method to execute, we need to call it explicitly)
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- TourPlanner.RestServer/Program.cs
+++ TourPlanner.RestServer/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using TourPlanner.RestServer.DAL;
+using TourPlanner.RestServer.DAL.Repository;
+using TourPlanner.RestServer.DAL.Repository.Interfaces;
+using TourPlanner.RestServer.Middleware;
 
 namespace TourPlanner.RestServer
 {
@@ -8,6 +13,14 @@ namespace TourPlanner.RestServer
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            
+            // Configure Dbcontext with PostgreSQL
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+            
+            // Register repositories
+            builder.Services.AddScoped<ITourRepository, TourRepository>();
+            builder.Services.AddScoped<ITourLogRepository, TourLogRepository>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +28,15 @@ namespace TourPlanner.RestServer
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+            
+            // Create a scope to obtain the AppDbContext instance and ensure the database is created.
+            using (var scope = app.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                context.Database.EnsureCreated();
+            }
+            
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
