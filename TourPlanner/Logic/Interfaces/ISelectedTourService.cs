0e5900f3f6c039c94bbb766b45d658a912e2f5a0
Davimalu <accounts@zeugner.dev>
2025-06-19 10:47:12 +0200

    Re-add missing interface member

--- TourPlanner/Logic/Interfaces/ISelectedTourService.cs
+++ TourPlanner/Logic/Interfaces/ISelectedTourService.cs
@@ -5,6 +5,7 @@ namespace TourPlanner.Logic.Interfaces
 {
     public interface ISelectedTourService
     {
+        Tour? SelectedTour { get; set; }
         /// <summary>
         /// Event that is raised when the selected tour changes.
         /// Provides the newly selected tour (or null if no tour is selected).
